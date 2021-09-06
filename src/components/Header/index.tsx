c5ac92b4a812298548c6c410b0645c38e4c92dca
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-09-06 10:40:56 -0300

    add: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from "./style"
+
+export function Header(){
+    return(
+        <>
+            <Container>
+                <Content>
+                    <img src={logoImg} alt="App logo - dt money"/>
+                    <button type="button" >
+                        Nova transação
+                    </button>
+                </Content>
+            </Container>
+        </>
+    )
+}
