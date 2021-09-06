a81e4ec669bcec519878d71acfcd23e82bdfce03
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-09-06 13:01:56 -0300

    add: dashboard and summary components

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -18,6 +18,15 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    p span button{
+        font-weight: 400;
+    }
+
+    h1 h2 h3 h4 h5 strong{
+        font-weight: 600;
     }
 
     html{
